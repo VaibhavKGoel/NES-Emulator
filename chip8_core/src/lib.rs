6b1aa4ebf2257ac175102b8aadef457d0723c7e0
Dev Shah <devshah580@gmail.com>
2025-11-13 22:17:45 -0600

    Changed variable names

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -28,47 +28,74 @@ const FONTSET: [u8; FONTSET_SIZE] = [
 ];
 
 pub struct Emu {
-    pc: u16,
+    program_counter: u16,
     ram:[u8; RAM_SIZE],
     screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
     v_reg: [u8; NUM_REGS],
     i_reg: u16,
-    sp: u16,
+    stack_pointer: u16,
     stack: [u16; STACK_SIZE],
     keys: [bool; NUM_KEYS],
-    dt: u8,
-    st: u8,
+    delay_timer: u8,
+    sound_timer: u8,
 }
 
 impl Emu {
     pub fn new() -> Self {
         let mut new_emu = Self {
-            pc: START_ADDR,
+            program_counter: START_ADDR,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
             v_reg: [0; NUM_REGS],
             i_reg: 0,
-            sp: 0,
+            stack_pointer: 0,
             stack: [0; STACK_SIZE],
             keys: [false; NUM_KEYS],
-            dt: 0,
-            st: 0,
+            delay_timer: 0,
+            sound_timer: 0,
         };
 
         for i in 0..FONTSET_SIZE {
             new_emu.ram[i] = FONTSET[i];
-            println!("{}", new_emu.ram[i]);
         }
 
         new_emu
     }
 
     fn push(&mut self, val: u16) {
-        self.stack[self.sp as usize] = val;
-        self.sp += 1;
+        self.stack[self.stack_pointer as usize] = val;
+        self.stack_pointer += 1;
     }
     fn pop(&mut self) -> u16 {
-        self.sp -= 1;
-        self.stack[self.sp as usize]
+        self.stack_pointer -= 1;
+        self.stack[self.stack_pointer as usize]
     }
+
+    pub fn reset(&mut self) {
+        self.program_counter = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.stack_pointer = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.delay_timer = 0;
+        self.sound_timer = 0;
+        for i in 0..FONTSET_SIZE {
+            self.ram[i] = FONTSET[i];
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let fetched = self.fetch();
+    }
+
+    fn fetch(&mut self) -> u8 {
+        let result: u8 = self.ram[self.program_counter as usize];
+        self.program_counter += 1;
+        
+        result
+    }
+    
 }
